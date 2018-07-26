951851e947b4244972752ba5241a63555ca4c437
LaurenWorkosky <19lworkosky@student.dasd.org>
2018-07-26 13:04:31 +0000

    automatically backed up by learn

--- index.rb
+++ index.rb
@@ -6,7 +6,7 @@ ingredients = [
     ["mayo", "mustard", "tabasco"]
 ]
 
-def sandwich_request(ingredients, ["sourdough", "tofu", "cheddar"]) 
+def sandwich_request(ingredients, ["sourdough", "turkey", ""]) 
   # your code here!
   =>
 end
